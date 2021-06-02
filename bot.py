30af19281c259f9c3eb49cd9f5fc57e8f824cbfc
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-06-02 12:12:51 +0530

    huh

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 #    Copyright (C) 2021 - Infinity Bots
-#    This programme is a part of JEAnonymousBot
+#    This programme is a part of JEBotZ
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
