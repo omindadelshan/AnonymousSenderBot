6128f2ad19c7a63a34ac2b982c354505f3d37d79
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-06-02 17:40:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -87,7 +87,7 @@ async def _(event):
 @JEBotZ.on(events.NewMessage(pattern="^/send ?(.*)"))
 async def caption(event):
    if event.is_group:
-      if event.is_admin:
+      if event is_admin:
         try:
           lel = await event.get_reply_message()
           cap = event.pattern_match.group(1)
